da1e188d6fb5aa23a3f0790bb0b446f621f8cf9a
sanjay.ganta <gantasanjay143@gmail.com>
2019-09-11 10:19:55 +0530

    Tst

---
+++ TEST/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gitRepo = @"https://github.com/97rajnish/CaseStudy.git";
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
+            cmdStartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            cmdStartInfo.RedirectStandardInput = true;
+            cmdStartInfo.UseShellExecute = false;
+            cmdStartInfo.CreateNoWindow = true;
+            string command = "git clone " + gitRepo + " C://Temp//CaseStudy1";
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo = cmdStartInfo;
+            cmdProcess.Start();
+            cmdProcess.StandardInput.WriteLine(command);
+            cmdProcess.StandardInput.WriteLine("exit");
+            cmdProcess.WaitForExit();
+        }
+    }
+}
---
+++ TEST/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            string gitRepo = @"https://github.com/97rajnish/CaseStudy.git";
+            ProcessStartInfo cmdStartInfo = new ProcessStartInfo();
+            cmdStartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            cmdStartInfo.RedirectStandardInput = true;
+            cmdStartInfo.UseShellExecute = false;
+            cmdStartInfo.CreateNoWindow = true;
+            string command = "git clone " + gitRepo + " C://Temp//CaseStudy1";
+            Process cmdProcess = new Process();
+            cmdProcess.StartInfo = cmdStartInfo;
+            cmdProcess.Start();
+            cmdProcess.StandardInput.WriteLine(command);
+            cmdProcess.StandardInput.WriteLine("exit");
+            cmdProcess.WaitForExit();
+        }
+    }
+}
